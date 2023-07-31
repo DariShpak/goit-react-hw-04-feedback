5787166c261adb116741fec05cece1ad3db53924
Daria Shpak <darya.shpak@gmail.com>
2023-07-31 16:31:45 +0300

    Refactoring component with React hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,65 @@
-import React from "react"
-import Statistics from "./Feedback/Statistics/Statistics"
+import {useState} from "react"
 import FeedbackOptions from "./Feedback/FeedbackOptions/FeedbackOptions"
 import Section from "./Feedback/Section/Section"
+import Statistics from "./Feedback/Statistics/Statistics"
 import NotificationMessage from "./Feedback/Statistics/NotificationMessage"
 import {Container} from "./Feedback/Feedback.styled"
 
-class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  }
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  onLeaveFeedback = option => {
-    this.setState(prevState => ({
-      [option]: prevState[option] + 1
-    }))
-  }
+  const addFeedback = option => {
+    switch (option) {
+      case "good":
+        setGood(prevState => prevState + 1)
+        break
 
-  countTotalFeedback = () => {
-    const {good, neutral, bad} = this.state
-    return good + neutral + bad
-  }
+      case "neutral":
+        setNeutral(prevState => prevState + 1)
+        break
 
-  countPositiveFeedbackPercentage() {
-    const {good} = this.state
-    const totalFeedback = this.countTotalFeedback()
+      case "bad":
+        setBad(prevState => prevState + 1)
+        break
+
+      default:
+        return
+    }
+  }
+  const options = {good, neutral, bad}
+
+  const totalFeedback = good + neutral + bad
+
+  const countPositivePercentage = () => {
     return totalFeedback > 0 ? Math.round(good / totalFeedback * 100) : 0
   }
 
-  render() {
-    const {good, neutral, bad} = this.state
-    const totalFeedback = this.countTotalFeedback()
-    const positiveFeedback = this.countPositiveFeedbackPercentage()
+  const positivePercentage = countPositivePercentage()
 
-    return (
-      <Container>
-        <Section title="please rate our service">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.onLeaveFeedback}
-          />
-        </Section>
-
-        <Section title="Statistics">
-          {totalFeedback === 0
-            ? <NotificationMessage message={"There is no feedback yet"} />
-            : <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={totalFeedback}
-                positivePercentage={positiveFeedback}
-              />}
-        </Section>
-      </Container>
-    )
-  }
+  return (
+    <Container>
+      <Section title="Please rate our service">
+        <FeedbackOptions
+          addFeedback={addFeedback}
+          options={Object.keys(options)}
+        />
+      </Section>
+      <Section title="Statistics">
+        {totalFeedback === 0
+          ? <NotificationMessage message={"There is no feedback yet"} />
+          : <Statistics
+            title="Statistics"
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
+          />}
+      </Section>
+    </Container>
+  )
 }
 
 export default App
